5a0eab999e303494fcbbbe6f6c8bb179099e31d1
HaymarnYH <haymarn1299@gmail.com>
2025-05-08 14:24:07 +0630

    Message

--- src/main/java/com/testproject/Database_Read.java
+++ src/main/java/com/testproject/Database_Read.java
@@ -3,6 +3,7 @@ package com.testproject;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Database_Read {
     private Connection con;
@@ -10,16 +11,19 @@ public class Database_Read {
     public Database_Read(Connection con) {
         this.con = con;
     }
-    protected void read(){
+
+    protected ArrayList<offices> read(){// testing a twt kg tk yay nee lay pr reuseable phit loh pr
+        ArrayList<offices> aloffice= new ArrayList<>();
         try{
             PreparedStatement ps = con.prepareStatement("SELECT * FROM offices WHERE country = ?");
             ps.setString(1,"USA");
             ResultSet rs = ps.executeQuery();
             while (rs.next()){
-                System.out.print(rs.getInt(1));
-                System.out.print(" | ");
-                System.out.print(rs.getString("city"));
-                System.out.println();
+                aloffice.add(new offices(rs.getString(1),
+                        rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5),
+                        rs.getString(6), rs.getString(7),
+                        rs.getString(8), rs.getString(9)));
             }
             rs.close();
             ps.close();
@@ -27,5 +31,10 @@ public class Database_Read {
         catch (Exception e){
             e.printStackTrace();
         }
+        return aloffice;
     }
+    protected void display(ArrayList<offices>aloffice){
+
+    }
+
 }
