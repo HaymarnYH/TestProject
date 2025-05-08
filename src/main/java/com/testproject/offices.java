111a5d77368ecce20d76500a2908bb166efe03bd
HaymarnYH <haymarn1299@gmail.com>
2025-05-08 14:25:35 +0630

    Message

---
+++ src/main/java/com/testproject/offices.java
@@ -0,0 +1,54 @@
+package com.testproject;
+
+public class offices {
+    private String officeCode, city, phone, addressLine1, addressLine2,  state, country, postalCode, territory;
+
+    public offices(String officeCode, String city, String phone, String addressLine1, String addressLine2, String state,
+                   String country, String postalCode, String territory) {
+        this.officeCode = officeCode;
+        this.city = city;
+        this.phone = phone;
+        this.addressLine1 = addressLine1;
+        this.addressLine2 = addressLine2;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
+        this.territory = territory;
+    }
+
+    public String getOfficeCode() {
+        return officeCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getTerritory() {
+        return territory;
+    }
+}
